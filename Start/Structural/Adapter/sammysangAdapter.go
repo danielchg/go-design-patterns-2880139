0a8bdb73af30d0211653b8c119f15fe182f733e2
Daniel Chavero <dchavero@redhat.com>
2022-05-23 22:22:06 +0200

    add adapter examples

--- Start/Structural/Adapter/sammysangAdapter.go
+++ Start/Structural/Adapter/sammysangAdapter.go
@@ -2,32 +2,48 @@ package main
 
 type sammysangAdapter struct {
 	// TODO: add a field for the SammysangTV reference
+	sstv *SammysangTV
 }
 
 func (ss *sammysangAdapter) turnOn() {
 	// TODO
+	ss.sstv.setOnState(true)
 }
 
 func (ss *sammysangAdapter) turnOff() {
 	// TODO
+	ss.sstv.setOnState(false)
 }
 
 func (ss *sammysangAdapter) volumeUp() int {
 	// TODO
+	vol := ss.sstv.getVolume() + 1
+	ss.sstv.setVolume(vol)
+	return vol
 }
 
 func (ss *sammysangAdapter) volumeDown() int {
 	// TODO
+	vol := ss.sstv.getVolume() - 1
+	ss.sstv.setVolume(vol)
+	return vol
 }
 
 func (ss *sammysangAdapter) channelUp() int {
 	// TODO
+	channel := ss.sstv.getChannel() + 1
+	ss.sstv.setChannel(channel)
+	return channel
 }
 
 func (ss *sammysangAdapter) channelDown() int {
 	// TODO
+	channel := ss.sstv.getChannel() - 1
+	ss.sstv.setChannel(channel)
+	return channel
 }
 
 func (ss *sammysangAdapter) goToChannel(ch int) {
 	// TODO
+	ss.sstv.setChannel(ch)
 }
